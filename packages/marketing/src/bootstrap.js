b499314cf4c0440c46989087c9d26da9ab26fdf3
Bharat Shah <akashche72@gmail.com>
2022-02-20 17:08:26 +0400

    Since in marketing it was using memoryHistory router while create app in both side in container and while running marketing in totally isolated mode and route was not working for marketing app because of createMemoryHistory now checking if marketing app is loaded from container or it's in isolated mode base on that creating history variable either createBroweserHistory or create memoryHisoty

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
 // Mout function to start up the app
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot, {});
+    mount(devRoot, {defaultHistory: createBrowserHistory()});
   }
 }
 
